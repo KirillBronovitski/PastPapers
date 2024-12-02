7c5b95ed1da0473cd68592224554cb7c5ec62535
Kirill Bronovitski <kirillbronovitski@gmail.com>
2024-12-02 16:57:16 +0100

    New solution for Paper1q1. Paper1q2 and Paper1q3 written with other related classes

--- src/main/java/org/example/Oct202342/Paper1q1.java
+++ src/main/java/org/example/Oct202342/Paper1q1.java
@@ -1,9 +1,112 @@
 package org.example.Oct202342;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Scanner;
 
+import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.Paths;
+/*  The commented imports are required for the previous solution (see commented code below)
+import java.nio.file.Files;
+*/
+public class Paper1q1 {
+    static char[] StackVowel = new char[100];
+    static char[] StackConsonant = new char[100];
+    static int VowelTop = 0;
+    static int ConsonantTop = 0;
+
+    public static void main(String[] args) throws IOException {
+        ReadData();
+        Scanner console = new Scanner(System.in);
+        String userChoice;
+        String letters = "";
+        String consonantPop;
+        String vowelPop;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Which letter, vowel or consonant, is required to be popped (write consonant or vowel)");
+            userChoice = console.nextLine();
+            if (userChoice.equals("consonant")) {
+                consonantPop = PopConsonant();
+                if (!consonantPop.equals("No data")) {
+                    letters = letters + consonantPop;
+                }
+            } else if (userChoice.equals("vowel")) {
+                vowelPop = PopVowel();
+                if (!vowelPop.equals("No data")) {
+                    letters = letters + vowelPop;
+                }
+            } else {
+                i--;
+            }
+        }
+        System.out.println(letters);
+    }
+
+    public static void PushData(char letter) {
+        boolean isVowel = letter == 'e' || letter == 'o' || letter == 'i' || letter == 'u' || letter == 'a';
+        if (isVowel) {
+            if (VowelTop < StackVowel.length) {
+                StackVowel[VowelTop] = letter;
+                VowelTop++;
+            } else {
+                System.out.println("The stack is full, the letter cannot be added.");
+            }
+
+        } else {
+            if (ConsonantTop < StackConsonant.length) {
+                StackConsonant[ConsonantTop] = letter;
+                ConsonantTop++;
+            } else {
+                System.out.println("The stack is full, the letter cannot be added.");
+            }
+        }
+    }
+
+    public static void ReadData() throws IOException {
+        String fileLine;
+        char letter;
+        int index = 0;
+        try (Scanner fileReader = new Scanner(Paths.get("C:/dev/StackData.txt"))) {
+            while (fileReader.hasNextLine()) {
+                fileLine = fileReader.nextLine();
+                while (index < fileLine.length()) {
+                    letter = fileLine.charAt(index);
+                    PushData(letter);
+                    index++;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("The source file had not been found or does not exist.");
+        }
+    }
+
+    public static String PopVowel() {
+        String returnValue = "No data";
+            if (StackVowel[0] == '\0') {
+                System.out.println("No item - the stack is empty.");
+                return returnValue;
+            }
+            if (VowelTop > 0) {
+                returnValue = String.valueOf(StackVowel[VowelTop - 1]);
+                StackVowel[VowelTop - 1] = '\0';
+                VowelTop--;
+            }
+        return returnValue;
+    }
+
+    public static String PopConsonant() {
+        String returnValue = "No data" ;
+        if (StackConsonant[0] == '\0') {
+            System.out.println("No item - the stack is empty.");
+            return returnValue;
+        }
+        if (ConsonantTop > 0) {
+            returnValue = String.valueOf(StackConsonant[ConsonantTop - 1]);
+            StackConsonant[ConsonantTop - 1] = '\0';
+            ConsonantTop--;
+        }
+        return returnValue;
+    }
+
+}
+/*                                  Previous solution (all the commented code below this text)
 public class Paper1q1 {
 
     char[] StackVowel = new char[100];
@@ -120,5 +223,5 @@ public class Paper1q1 {
 
     }
 }
-
+*/
 
